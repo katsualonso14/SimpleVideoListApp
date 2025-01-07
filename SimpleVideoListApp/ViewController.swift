6c508f8509e271cb4265072e7977271449708e59
katsuyatamai <xabi.k0124football@gmail.com>
2025-01-07 12:48:57 +0100

    kickoff

---
+++ SimpleVideoListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleVideoListApp
+//
+//  Created by KatsuyaTamai on 2025/01/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
